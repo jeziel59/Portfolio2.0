2f827302c32abe4971c174331f0cd492d6428714
Jeziel Silva <gearqq@hotmail.com>
2023-05-30 14:11:52 -0300

    adicionado slides

---
+++ Javascript/modules/slide.js
@@ -0,0 +1,233 @@
+import debounce from "./debounce.js";
+
+export class Slide {
+  constructor(slide, wrapper) {
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 };
+    this.activeClass = "active";
+    this.changeEvent = new Event("changeEvent");
+  }
+
+  //Metodo: adiciona transição caso true
+  transition(active) {
+    this.slide.style.transition = active ? "transform .3s" : "";
+  }
+
+  //1° Metodo: Os seguintes eventos, inicia metodo de start ou end
+  addSlideEvent() {
+    this.wrapper.addEventListener("mousedown", this.onStart); //
+    this.wrapper.addEventListener("touchstart", this.onStart);
+    this.wrapper.addEventListener("mouseup", this.onEnd);
+    this.wrapper.addEventListener("touchend", this.onEnd);
+  }
+
+  //2° Metodo: metodo que salva no objeto onde foi o clique, e aciona o metodo de medir a distancia de movimento
+  onStart(event) {
+    let moveType;
+    if (event.type === "mousedown") {
+      event.preventDefault();
+      this.dist.startX = event.clientX;
+      moveType = "mousemove";
+    } else {
+      this.dist.startX = event.changedTouches[0].clientX;
+      moveType = "touchmove";
+    }
+    this.wrapper.addEventListener(moveType, this.onMove);
+    this.transition(false);
+  }
+
+  //3° Metodo: recebe o movimento do mouse, e ativa metodo de calculo da quantidade de movimento
+  onMove(event) {
+    const pointerPosition =
+      event.type === "mousemove"
+        ? event.clientX
+        : event.changedTouches[0].clientX;
+    const finalPosition = this.updatePosition(pointerPosition);
+    this.moveSlide(finalPosition);
+  }
+  // Calcula o movimento do mouse
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+    return this.dist.finalPosition - this.dist.movement;
+  }
+  //
+
+  //4° Metodo: Move o slide de acordo com o movimento do mouse passado como parametro durante o clique
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  //5° Metodo - quando ocorrer o mouseup, é removido o evento de move, salvo a posição no objeto e acionado a transição e troca de slide
+  onEnd(event) {
+    const moveType = event.type === "mouseup" ? "mousemove" : "touchmove";
+    this.wrapper.removeEventListener(moveType, this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
+    this.transition(true);
+    this.changeSlideOnEnd();
+  }
+  //6° Metodo - metodo que verifica se o movimento foi acima de 120, caso sim aciona os metodos de transição
+  changeSlideOnEnd() {
+    if (this.dist.movement > 120 && this.index.next !== undefined) {
+      this.activeNextSlide(); // aciona o proximo slide
+    } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active);
+    }
+  }
+  //slide config - Configurações que salvam as posições
+  changeSlide(index) {
+    const activeSlide = this.slideArray[index];
+    this.moveSlide(activeSlide.position);
+    this.slidesIndexNav(index);
+    this.dist.finalPosition = activeSlide.position;
+    this.changeActiveClass();
+    this.wrapper.dispatchEvent(this.changeEvent);
+  }
+
+  //1° metodo em init - Metodo que cria slideArray, verifica o centro da tela e retorna
+  slidesConfig() {
+    this.slideArray = [...this.slide.children].map((element) => {
+      const position = this.slidePosition(element);
+      return {
+        position,
+        element,
+      };
+    });
+  }
+  // metodo que retorna o centro do wrap
+  slidePosition(slide) {
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+    return -(slide.offsetLeft - margin);
+  }
+
+  // metodo que define o slide atual, proximo e anterior
+  slidesIndexNav(index) {
+    const last = this.slideArray.length - 1;
+    this.index = {
+      prev: index ? index - 1 : undefined,
+      active: index,
+      next: index === last ? undefined : index + 1,
+    };
+  }
+
+  //Navegação - caso for diferente de undefined, ativa a troca de slide
+  activePrevSlide() {
+    if (this.index.prev !== undefined) {
+      this.changeSlide(this.index.prev);
+    }
+  }
+  activeNextSlide() {
+    if (this.index.next !== undefined) {
+      this.changeSlide(this.index.next);
+    }
+  }
+  //
+
+  //Metodo que troca a classe do item ativo
+  changeActiveClass() {
+    this.slideArray.forEach((item) =>
+      item.element.classList.remove(this.activeClass)
+    );
+    this.slideArray[this.index.active].element.classList.add(this.activeClass);
+  }
+
+  //Quando houver mudança de tela, ativa a função que verifica o centro da tela
+  onResize() {
+    setTimeout(() => {
+      this.slidesConfig();
+      this.changeSlide(this.index.active);
+    }, 1000);
+  }
+
+  //Init - função que verifica se houve mudança de tamanho da janela
+  addResizeEvent() {
+    window.addEventListener("resize", this.onResize);
+  }
+
+  //Bind dos eventos
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+    (this.onResize = debounce(this.onResize.bind(this))), 200;
+    this.activePrevSlide = this.activePrevSlide.bind(this);
+    this.activeNextSlide = this.activeNextSlide.bind(this);
+  }
+
+  init() {
+    this.bindEvents();
+    this.transition(true);
+    this.addSlideEvent();
+    this.slidesConfig();
+    this.addResizeEvent();
+    this.changeSlide(0);
+    return this;
+  }
+}
+
+// metodos de navegação
+export default class SlideNav extends Slide {
+  constructor(slide, wrapper) {
+    super(slide, wrapper);
+    this.bindControlEvents();
+  }
+
+  // não utilizado, metodo que adiciona botoes de prev e next
+  addArrow(prev, next) {
+    this.prevElement = document.querySelector(prev);
+    this.nextElement = document.querySelector(next);
+    this.addArrowEvent();
+  }
+
+  addArrowEvent() {
+    this.prevElement.addEventListener("click", this.activePrevSlide);
+    this.nextElement.addEventListener("click", this.activeNextSlide);
+  }
+  //Cria os controles de nagegação caso não sejam passados
+  createControl() {
+    const control = document.createElement("ul");
+    control.dataset.control = "slide";
+    this.slideArray.forEach((item, index) => {
+      control.innerHTML += `<li><a href="#slide${index + 1}">${
+        index + 1
+      }</a></li>`;
+    });
+    this.wrapper.appendChild(control);
+    return control;
+  }
+
+  //Evento de click na nagegação ativa a troca de slide
+  eventControl(item, index) {
+    item.addEventListener("click", (event) => {
+      event.preventDefault();
+      this.changeSlide(index);
+    });
+    this.wrapper.addEventListener("changeEvent", this.activeControlItem);
+  }
+
+  //Adiciona classe ativa ao controle referente ao index ativo
+  activeControlItem() {
+    this.controlArray.forEach((item) =>
+      item.classList.remove(this.activeClass)
+    );
+    this.controlArray[this.index.active].classList.add(this.activeClass);
+  }
+
+  //Caso não seja passado a lista de navegação, é criado uma
+  addControl(customControl) {
+    this.control =
+      document.querySelector(customControl) || this.createControl();
+    this.controlArray = [...this.control.children];
+    this.controlArray.forEach(this.eventControl);
+    this.activeControlItem();
+  }
+
+  //Bind dos controles
+  bindControlEvents() {
+    this.eventControl = this.eventControl.bind(this);
+    this.activeControlItem = this.activeControlItem.bind(this);
+  }
+}
