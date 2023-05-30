558fa4b9d886c7a20a691402fb3283aa3619cbd9
Jeziel Silva <gearqq@hotmail.com>
2023-05-30 17:32:05 -0300

    adicionado animacao ao scroll

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,19 +1,20 @@
 import MenuMobile from "./modules/menu-mobile.js";
+import ScrollSuave from "./modules/scroll-suave.js";
+import moveProject from "./modules/moveProject.js";
+import SlideNav from "./modules/slide.js";
+import ScrollAnima from "./modules/ScrollAnima.js";
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
-
-import ScrollSuave from "./modules/scroll-suave.js";
 
 const scrollsuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollsuave.init();
 
-import moveProject from "./modules/moveProject.js";
+const moveproject = new moveProject(".titulosH2", ".content-project");
+moveproject.init();
 
- const moveproject = new moveProject('.titulosH2', '.content-project');
- moveproject.init();
+const slide = new SlideNav(".slide", ".slide-wrapper");
+slide.init();
+slide.addControl(".custom-controls");
 
- import SlideNav from './modules/slide.js'
-
- const slide = new SlideNav('.slide', '.slide-wrapper')
-slide.init()
-slide.addControl('.custom-controls');
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]')
+scrollAnima.init()
