973266d687304c21fb89c19006e169a91cd08c4f
Jeziel Silva <gearqq@hotmail.com>
2023-11-28 23:07:16 -0300

    Atualização menu mobile

--- Javascript/modules/menu-mobile.js
+++ Javascript/modules/menu-mobile.js
@@ -14,12 +14,22 @@ export default class MenuMobile {
   openMenu(event) {
     event.preventDefault();
     this.menuList.classList.add(this.classActive);
-    this.menuButton.classList.add(this.classActive);
+    this.menuButton.classList.add(this.classActive);  
+    const removeClassesAfterDelay = () => {
+      setTimeout(() => {
+        this.menuList.classList.remove(this.classActive);
+        this.menuButton.classList.remove(this.classActive);
+        this.menuList.classList.remove('Inactive');
+        this.menuButton.classList.remove('Inactive');
+      }, 300);
+    };  
     outsideClick(this.menuList, this.events, () => {
-      this.menuList.classList.remove(this.classActive);
-      this.menuButton.classList.remove(this.classActive);
+      this.menuList.classList.add('Inactive');
+      this.menuButton.classList.add('Inactive');
+      removeClassesAfterDelay();
     });
   }
+  
 
   addEventOpenMenu() {
     if (this.menuButton)
